50b03395534500f1d2c4bc8248501f993d6e585e
Irtaza0420BM <m.irtazabinashar@gmail.com>
2025-06-21 20:01:33 +0500

    asdas

--- backend_api/config/cors.php
+++ backend_api/config/cors.php
@@ -19,8 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
+'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
@@ -29,6 +28,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 
 ]; 
